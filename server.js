d6335cdb7bf2940e7d5b47b18743dfa3ab9bdf53
Zanzong43 <alexkoszuta43@gmail.com>
2021-07-24 20:21:09 -0400

    deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookies = require("cookie-parser");
+const path = require('path')
 
 
 
@@ -24,6 +25,11 @@ require('./server/config/mongoose');
 
 require('./server/routes/routes')(app);
 
+app.use(express.static('client/build'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
 
 
 app.listen(port, () => {
