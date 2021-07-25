6a5f6008e024b5cb08f369edcc87eabda2ddb865
Zanzong43 <alexkoszuta43@gmail.com>
2021-07-25 13:53:38 -0400

    removed homepage from packagejson

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const cookies = require("cookie-parser");
-const path = require('path')
 
 
 
@@ -25,10 +24,17 @@ require('./server/config/mongoose');
 
 require('./server/routes/routes')(app);
 
-app.get('*', (req, res) => {
-    const index = path.join(__dirname, 'client', 'build', 'index.html');
-    res.sendFile(index);
-});
+
+
+
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
+
+
+
 
 
 app.listen(process.env.PORT || port, () => {
